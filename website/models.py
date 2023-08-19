afec50253cfcb9967617362cdc8d649ac4f19bf1
Antara Kundu <antara_kundu@persistent.com>
2023-08-19 23:43:23 +0530

    added My cart page

--- website/models.py
+++ website/models.py
@@ -14,3 +14,9 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     fullname = db.Column(db.String(150))
+
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
--- website/views.py
+++ website/views.py
@@ -1,48 +1,95 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for, flash
 from flask_login import login_user, login_required, logout_user, current_user
-from . import models
-from .models import Product
+from . import models, db
+from .models import Product, User, CartItem
+from collections import defaultdict
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
-
 def home():
     products = Product.query.all()
     return render_template("home.html",products=products, user=current_user)
-
 
 @views.route('/product/<int:product_id>')
 def product_detail(product_id):
     product = Product.query.get(product_id)
     return render_template("product_detail.html",product=product, user=current_user)
 
-@views.route('/mycart/<int:product_id>')
+@views.route('/shopping_cart/<int:product_id>')
 def shopping_cart(product_id):
-    product = next((p for p in products if p['id'] == product_id), None)
-    if product:
-        if 'cart' not in session:
-            session['cart'] = []
-        session['cart'].append(product)
-        return redirect((url_for('home')))
-    else:
-        return 'Product not found'
+    if not current_user.is_authenticated:
+        return redirect(url_for("auth.login"))
 
-# @views.route('/cart')
-# def add_to_cart(product_id, quantity):
-#     product = Product.query.get(product_id)
-#     if product:
-#         # Get or create the user's cart
-#         cart = session.get('cart',{})
+    user_id = session['_user_id']
+    user = User.query.get(user_id)
+    product = Product.query.get(product_id)
 
-#         # Add the product to the cart
-#         if product_id in cart:
-#             cart[product_id] +=quantity
-#         else:
-#             cart[product_id] = quantity
+    if user and product:
+        cart_item = CartItem.query.filter_by(user_id=user_id, product_id=product_id).first()
         
-#         session['cart'] = cart
-#         flash(f'{product.name} added to cart.', 'success')
+        if cart_item:
+            cart_item.quantity+=1
+        else:
+            cart_item=CartItem(user_id=user_id, product_id=product_id, quantity=1)
+            db.session.add(cart_item)
     
-#     else:
-#         flash('Product not found.', 'danger')
+        db.session.commit()
+        flash(f'{product.name} has been added to cart successfully!', 'success')
+    else:
+        flash(f'Error adding product to cart', 'danger')
+    return redirect(url_for('views.home'))
+
+@views.route('/mycart')
+def mycart():
+    if not current_user.is_authenticated:
+        flash('Please login to view your cart', 'warning')
+        return redirect(url_for("auth.login"))
+    
+    user_id = session['_user_id']
+    user = User.query.get(user_id)
+    cart_item = CartItem.query.filter_by(user_id=user_id).all()
+    total=0
+    product_dict=defaultdict(Product)
+    for item in cart_item:
+        product = Product.query.get(item.product_id)
+        product_dict[item]=product
+        total += product.price*item.quantity
+    return render_template('mycart.html', product_dict=product_dict, total=total, user=current_user)
+
+
+@views.route('/remove_from_cart/<int:item_id>')
+def remove_from_cart(item_id):
+    if current_user.is_authenticated():
+        cart_item = CartItem.query.get(item_id=item_id)
+        if cart_item:
+            db.session.delete(cart_item)
+            db.session.commit()
+            flash('Item removed from cart.', 'success')
+        else:
+            flash('Item not found.', 'danger')
+    return redirect(url_for('views.mycart'))
+
+@views.route('/checkout')
+def checkout():
+    if current_user.is_authenticated:
+        return render_template('checkout.html')
+
+@views.route('/remove_one_from_cart/<int:item_id>')
+def remove_one_from_cart(item_id):
+    if current_user.is_authenticated:
+        user_id = session['_user_id']
+        cart_item = CartItem.query.filter_by
+
+        if cart_item:
+            if cart_item.quantity > 1:
+                cart_item.quantity -=1
+                db.session.commit()
+            else:
+                db.session.delete(cart_item)
+                db.session.commit()
+                flash('Item removed from cart.', 'success')
+        else:
+            flash('Item not found in cart.', 'danger')
+    
+    return redirect(url_for('views.mycart'))
